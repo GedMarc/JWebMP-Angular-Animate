854a8c52e57692b8c850dd0f2fcdad3b16082535
Marc Magon <ged_marc@hotmail.com>
2017-11-30 20:39:28 +0200

    Pom file cleanup, maven dependency plugin removed????

--- src/za/co/mmagon/jwebswing/plugins/angularanimate/AngularAnimatePageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/angularanimate/AngularAnimatePageConfigurator.java
@@ -10,12 +10,12 @@ import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
  * @author Marc Magon
  * @since 08 Jun 2017
  */
-@PluginInformation(pluginName = "Angular Toastr",
-		pluginDescription = "These animation hooks are set in place to trigger animations during the life cycle of various directives and when triggered, will attempt to perform a CSS Transition, CSS Keyframe Animation or a JavaScript callback Animation (depending on whether an animation is placed on the given directive). Animations can be placed using vanilla CSS by following the naming conventions set in place by AngularJS or with JavaScript code, defined as a factory.",
+@PluginInformation(pluginName = "Angular Animate",
+		pluginDescription = "These animation hooks in the angular animation for the project.",
 		pluginUniqueName = "jwebswing-angular-animate",
 		pluginVersion = "1.6.4",
 		pluginCategories = "angular,animations, ui,web ui, framework",
-		pluginSubtitle = "hese animation hooks are set in place to trigger animations during the life cycle of various directives ",
+		pluginSubtitle = "these animation hooks are set in place to trigger animations during the life cycle of various directives ",
 		pluginSourceUrl = "https://docs.angularjs.org/guide/animations",
 		pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-Angular-Animate/wiki",
 		pluginGitUrl = "https://github.com/GedMarc/JWebSwing-Angular-Animate",
@@ -23,11 +23,12 @@ import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
 		pluginIconImageUrl = "",
 		pluginOriginalHomepage = "https://docs.angularjs.org/guide/animations",
 		pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/AngularAnimate.jar/download"
-) class AngularAnimatePageConfigurator extends PageConfigurator
+)
+public class AngularAnimatePageConfigurator extends PageConfigurator
 {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	/*
 	 * Constructs a new AngularAnimatePageConfigurator
 	 */
@@ -35,7 +36,7 @@ import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
 	{
 		//Nothing needed
 	}
-	
+
 	@Override
 	public Page configure(Page page)
 	{
