bd54ad9c393a36b0a8ee44be20b68790b2515a2f
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:21 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

---
+++ src/main/java/com/jwebmp/plugins/angularanimate/injections/AngularAnimateExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.angularanimate.injections;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AngularAnimateExclusionsModule
+		implements IGuiceScanModuleExclusions<AngularAnimateExclusionsModule>,
+				           IGuiceScanJarExclusions<AngularAnimateExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-angular-animate-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.angularanimate");
+		return strings;
+	}
+}
