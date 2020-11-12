9bc6f2c99a195159f13d92ac61b93ef8522f370d
GedMarc <ged_marc@hotmail.com>
2020-11-13 00:39:12 +0200

    module name java.validation to jakarta.validation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -2,7 +2,7 @@ module com.jwebmp.plugins.angularanimate {
 	exports com.jwebmp.plugins.angularanimate;
 
 	requires com.jwebmp.core;
-	requires java.validation;
+	requires jakarta.validation;
 	requires com.guicedee.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
